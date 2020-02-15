6ff340ef15d54b0a9af41908736aa68e607e88fe
jesslane94 <jesslane94@gmail.com>
2020-02-15 12:33:58 -0800

    sql file for creating tables
    SQL file for implementing the creation of the tables.

---
+++ create_tables.sql
@@ -0,0 +1,76 @@
+/*
+drop statements to clear out old records if they exist
+*/
+
+DROP TABLE IF EXISTS characters;
+DROP TABLE IF EXISTS guilds;
+DROP TABLE IF EXISTS spells;
+DROP TABLE IF EXISTS classes;
+DROP TABLE IF EXISTS classes_spells;
+DROP TABLE IF EXISTS characters_spells;
+
+/*
+create tables
+*/
+
+CREATE TABLE characters (
+    char_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    strength INT(11) NOT NULL,
+    dexterity INT(11) NOT NULL,
+    endurance INT(11) NOT NULL,
+    intelligence INT(11) NOT NULL,
+    class_id INT(11) NOT NULL,
+    guild_id INT(11),
+    FOREIGN KEY(class_id) REFERENCES classes(class_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY(guild_id) REFERENCES guilds(guild_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE spells (
+    spell_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    spell_name VARCHAR(255) NOT NULL,
+    spell_level VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE guilds (
+   guild_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   guild_name VARCHAR(255) NOT NULL,
+   guild_specialization VARCHAR(255) NOT NULL,
+   char_id int(11) NOT NULL
+   FOREIGN KEY(char_id) REFERENCES characters(char_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE, 
+);
+
+CREATE TABLE classes (
+   class_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   class_name VARCHAR(255) NOT NULL,
+   stat_bonus VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE classes_spells (
+    class_id INT(11) NOT NULL,
+    spell_id INT(11) NOT NULL,
+    FOREIGN KEY(class_id) REFERENCES classes(class_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY(spell_id) REFERENCES spells(spell_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+);
+
+CREATE TABLE characters_spells (
+    char_id INT(11) NOT NULL,
+    spell_id INT(11) NOT NULL,
+    FOREIGN KEY(char_id) REFERENCES characters(char_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY(spell_id) REFERENCES spells(spell_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+);
