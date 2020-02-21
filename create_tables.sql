a826bc4ef8b2796f8d0079ade8f72e498cbc1a8c
KurtKuhn <KuhnKu@oregonstate.edu>
2020-02-21 11:26:19 -0800

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -45,7 +45,7 @@ CREATE TABLE classes (
    class_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
    class_name VARCHAR(255) NOT NULL,
    stat_bonus VARCHAR(255) NOT NULL,
-   char_id int(11),
+   char_id int(11) NOT NULL,
    FOREIGN KEY(char_id) REFERENCES characters(char_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
@@ -74,22 +74,22 @@ CREATE TABLE characters_spells (
 );
 
 /*Insert Values into the Tables */
-INSERT INTO characters (first_name, last_name, strength, dexterity, endurance, intelligence)
-VALUES ("Grey", "Skull", 12, 15, 13, 8),
+INSERT INTO characters (:first_name, :last_name, :strength, :dexterity, :endurance, :intelligence)
+VALUES ("Grey", "Skull", 1/2, 15, 13, 8),
        ("Dryany", "Enyn", 8, 12, 8, 15),
        ("Adriel", "Serielye", 10, 15, 12, 11);
 
-INSERT INTO spells (spell_name, spell_level, spell_description) 
+INSERT INTO spells (:spell_name, :spell_level, :spell_description) 
 VALUES ("Fireball", "Level 1", "Shoots a fireball. Caution: It's hot."),
     ("Healing Touch", "Level 1", "Restores 5-10 HP to Target."), 
     ("Cast Light", "Level 1", "Makes an object you touch glow. Yippee!");
 
-INSERT INTO guilds (guild_name, guild_specialization)
+INSERT INTO guilds (:guild_name, :guild_specialization)
 VALUES ("Merchants", "You like? You buy! I give you a good price"),
        ("Mages", "Never trust a mage"),
        ("Dark Brotherhood", "I just want a nightmare'ish looking horse to ride on");
 
-INSERT INTO classes (class_name, stat_bonus) 
+INSERT INTO classes (:class_name, :stat_bonus) 
 VALUES ("Rogue", "+2 Dexterity"),
     ("Bard","+2 Charisma"), 
     ("Fighter", "+2 Strength");
